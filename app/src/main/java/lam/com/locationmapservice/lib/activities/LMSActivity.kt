ddf4d3abc89519569afebdca7c6b377fb35ab8c0
lam <lam@amsiq.com>
2018-06-13 18:29:14 +0200

    Maps: Added Google Maps

---
+++ app/src/main/java/lam/com/locationmapservice/lib/activities/LMSActivity.kt
@@ -0,0 +1,7 @@
+package lam.com.locationmapservice.lib.activities
+
+import com.trello.rxlifecycle2.components.support.RxAppCompatActivity
+import org.androidannotations.annotations.EActivity
+
+@EActivity
+abstract class LMSActivity: RxAppCompatActivity()

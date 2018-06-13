f568a7899faa57af952e826fe5208d726a5af840
BuildTools <unconfigured@null.spigotmc.org>
2018-06-13 12:32:49 +0200

    Initial commit

---
+++ app/src/main/java/lam/com/locationmapservice/StartUpActivity.kt
@@ -0,0 +1,12 @@
+package lam.com.locationmapservice
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class StartUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_up)
+    }
+}

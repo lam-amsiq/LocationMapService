d86b4b666a8c627093a6661ee2b55c7c9ebcd09f
lam <lam@amsiq.com>
2018-07-16 16:55:09 +0200

    Lib: Heatmap - Added mapping algorithm for pairing annotations by distance

--- app/src/main/java/lam/com/locationmapservice/lib/utils/HeatmapMaths.kt
+++ app/src/main/java/lam/com/locationmapservice/lib/utils/HeatmapMaths.kt
@@ -1,12 +1,57 @@
 package lam.com.locationmapservice.lib.utils
 
+import lam.com.locationmapservice.lib.models.Annotation
 import lam.com.locationmapservice.lib.models.Location
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 object HeatmapMaths {
-    fun getDistance(location1: Location, location2: Location): Double? {
-        location1.lng?.let { x1 ->
+    fun mapAnnotations(annotationList: LinkedList<Annotation>, groupDistance: Double, isSortedByLatitudeAsc: Boolean): HashMap<Long, ArrayList<Long>> {
+        val matchMap = hashMapOf<Long, ArrayList<Long>>()
+
+        if (!isSortedByLatitudeAsc) {
+            //TODO: Sort list by latitude ascending
+        }
+
+        var annotationListIterator: MutableListIterator<Annotation>
+        var next: Annotation
+        annotationList.forEachIndexed { i, current ->
+            if (current.position?.lat == null || current.position?.lng == null) return@forEachIndexed
+
+            matchMap[current.annotation_id] = arrayListOf()
+            annotationListIterator = annotationList.listIterator(i)
+
+            if (annotationListIterator.hasNext()) {
+                annotationListIterator.next()
+            } else {
+                return@forEachIndexed
+            }
+
+            while (annotationListIterator.hasNext()) {
+                next = annotationListIterator.next()
+
+                if (next.position?.lat?.let { nextY ->
+                            nextY - current.position?.lat!!
+                        } ?: continue <= groupDistance) {
+
+                    HeatmapMaths.getDistance(current.position, next.position)?.let { distance ->
+                        if (distance < groupDistance) {
+                            matchMap[current.annotation_id]?.add(next.annotation_id)
+                        }
+                    }
+                } else {
+                    break // Break if latitude distance is longer than group distance
+                }
+            }
+        }
+        return matchMap
+    }
+
+    fun getDistance(location1: Location?, location2: Location?): Double? {
+        location1?.lng?.let { x1 ->
             location1.lat?.let { y1 ->
-                location2.lng?.let { x2 ->
+                location2?.lng?.let { x2 ->
                     location2.lat?.let { y2 ->
                         return Math.sqrt(Math.pow(x2 - x1, 2.0) + Math.pow(y2 - y1, 2.0))
                     }
--- app/src/test/java/lam/com/locationmapservice/lib/utils/HeatmapMathsTest.kt
+++ app/src/test/java/lam/com/locationmapservice/lib/utils/HeatmapMathsTest.kt
@@ -1,9 +1,11 @@
 package lam.com.locationmapservice.lib.utils
 
+import lam.com.locationmapservice.lib.models.Annotation
 import lam.com.locationmapservice.lib.models.Location
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.util.*
 
 class HeatmapMathsTest {
     @Test
@@ -47,5 +49,42 @@ class HeatmapMathsTest {
         location2 = Location(0.0, 4.01)
         actual = HeatmapMaths.getDistance(location1, location2)
         assertNull(actual)
+    }
+
+    @Test
+    fun computeHeatmaps() {
+        val annotations = LinkedList(arrayListOf(
+                Annotation(10, "Joanne Rowling", null, Location(55.628,12.082)),
+                Annotation(9, "Irma Pince", null, Location(55.6336,12.0837)),
+                Annotation(11, "Katie Bell", null, Location(55.6427,12.0793)),
+                Annotation(12, "Luna Lovegood", null, Location(55.6505,12.0789)),
+                Annotation(6, "Fleur Delacour", null, Location(55.6733,12.5658)),
+                Annotation(5, "Emmeline Vance", null, Location(55.6736,12.5679)),
+                Annotation(8, "Hermione Granger", null, Location(55.6749,12.5641)),
+                Annotation(7, "Ginny Weasley", null, Location(55.6763,12.5683)),
+                Annotation(4, "Dolores Umbridge", null, Location(55.72,12.425)),
+                Annotation(1, "Amelia Bones", null, Location(55.725,12.4375)),
+                Annotation(2, "Bellatrix Lestrange", null, Location(55.735,12.437)),
+                Annotation(3, "Cho Chang", null, Location(55.74,12.44)),
+                Annotation(13, "Minerva McGonagall", null, Location(56.1266,12.3085))))
+
+        val expected = hashMapOf(
+                1L to arrayListOf(2L,3L),
+                2L to arrayListOf(3L),
+                3L to arrayListOf(),
+                4L to arrayListOf(1L,2L,3L),
+                5L to arrayListOf(8L,7L),
+                6L to arrayListOf(5L,8L,7L),
+                7L to arrayListOf(),
+                8L to arrayListOf(7L),
+                9L to arrayListOf(11L,12L),
+                10L to arrayListOf(9L,11L,12L),
+                11L to arrayListOf(12L),
+                12L to arrayListOf(),
+                13L to arrayListOf())
+
+        val actual = HeatmapMaths.mapAnnotations(annotations, .05, true)
+
+        assertEquals(expected, actual)
     }
 }
